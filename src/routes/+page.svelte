3650d7e6d5e9237a8e46233860568341dd4f1c77
Phantomlsh <phantomlsh@outlook.com>
2023-06-09 16:07:11 -0700

    Add /
    Add AceUI, Swal2

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,29 @@
 <script>
-let cot = 0
+  import bannerImg from '$lib/images/banner.svg'
+  import logoImg from '$lib/images/logo.svg'
+  import { goto } from '$app/navigation'
+  import swal from 'sweetalert2'
 
-function inc () {
-  cot++
-}
+  function login () {
+    goto('/login')
+  }
+
+  let cot = 0
+  function hit () {
+    cot++
+    if (cot >= 5) {
+      swal.fire('Developer Mode!', 'You have entered the developer mode', 'success')
+      window.localStorage.developer = 1
+    }
+    setTimeout(() => { cot-- }, 2000)
+  }
 </script>
 
-<p>cot: {cot}</p>
-<button on:click={inc}>Click Me</button>
+<section class="w-full h-screen relative bg-black text-white flex flex-col items-center justify-center">
+  <img src={bannerImg} class="absolute w-full left-0 object-cover opacity-30" style="height: 120vh; top: -20vh;">
+  <img on:click={hit} src={logoImg} class="absolute w-20 h-20 top-2 left-2">
+  <div class="relative flex flex-col items-center">
+    <h1 class="font-bold text-6xl sm:text-8xl">信息学堂</h1>
+    <button on:click={login} class="bg-white shadow transition-all hover:shadow-md px-4 py-2 rounded text-xl text-gray-700 font-bold hover:px-6 my-10">登录</button>
+  </div>
+</section>
