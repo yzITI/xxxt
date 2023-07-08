c8ff22317c384b3a1266ce803224ed236957b843
Phantomlsh <phantomlsh@outlook.com>
2023-07-08 17:36:10 +0800

    Complete donate page

--- src/routes/donate/+page.svelte
+++ src/routes/donate/+page.svelte
@@ -1,1 +1,10 @@
-<p>页面还在建设中！</p>
+<script>
+  import wxpay from '$lib/images/wxpay.png'
+  import alipay from '$lib/images/alipay.png'
+</script>
+
+<div class="w-full h-screen bg-black flex flex-col sm:flex-row items-center justify-center bg-gradient-to-b sm:bg-gradient-to-r from-green-900 to-blue-900">
+  <img class="w-64" src={wxpay}>
+  <div class="h-20 w-40" />
+  <img class="w-64" src={alipay}>
+</div>
